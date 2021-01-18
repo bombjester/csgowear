0a85338f5bed154cb0ae7a8400c304af48546e65
Eric Lee <e.keith.lee@gmail.com>
2021-01-18 13:54:55 -0800

    mongo atlas connection migration

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 // require file-system so that we can load, read, require all of the model files
 var fs = require('fs');
 // connect to the database
-mongoose.connect('admin:password@ds235785.mlab.com:35785/csgowear');
+// mongoose.connect('admin:password@ds235785.mlab.com:35785/csgowear');
+mongoose.connect('mongodb://admin:password@csgowear-shard-00-00.tkay1.mongodb.net:27017,csgowear-shard-00-01.tkay1.mongodb.net:27017,csgowear-shard-00-02.tkay1.mongodb.net:27017/csgowear?ssl=true&replicaSet=atlas-dlw34z-shard-0&authSource=admin&retryWrites=true&w=majority');
 // specify the path to all of the models
 var models_path = __dirname + '/../models'
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
